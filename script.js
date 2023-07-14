e5b5899c1fa459872c3e294c67f4d0d6538ec310
Daniel Leon <dalobusiness01@gmail.com>
2023-07-14 14:41:03 -0500

    [FEAT] Add conversion

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
 "use strict";
+
+const inputs = [
+  {
+    element: document.getElementById("decimalInput"),
+    base: 10,
+  },
+  {
+    element: document.getElementById("binaryInput"),
+    base: 2,
+  },
+  {
+    element: document.getElementById("octalInput"),
+    base: 8,
+  },
+  {
+    element: document.getElementById("hexInput"),
+    base: 16,
+  },
+];
+
+const numberConversion = function (number, base) {
+  return number.toString(base);
+};
+
+for (let input of inputs) {
+  input.element.addEventListener("input", () => {
+    const decimalNumber = parseInt(input.element.value, input.base);
+    for (let numberInput of inputs) {
+      numberInput.element.value = numberConversion(
+        decimalNumber,
+        numberInput.base
+      );
+    }
+  });
+}
